a45df3a6f26ac860bade5b5019435458e642790b
Matias <elnegrochoque@gmail.com>
2021-05-01 22:46:36 -0300

    se hizo la verificacion de que el usuario existe

--- src/App.js
+++ src/App.js
@@ -4,18 +4,48 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Inicio from "./components/Inicio";
 import InicioEval from './components/alumno/InicioEval';
 import Navegacion from './components/common/Navegacion';
+import Admin from './components/administrador/Admin';
+import { useState, useEffect } from 'react';
+
+
+
 function App() {
+  
+  const URL = process.env.REACT_APP_API_URL;
+  const [personas, setPersonas] = useState([]);
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    try {
+      const consulta = await fetch(URL);
+      const respuesta = await consulta.json();
+
+      setPersonas(respuesta);
+
+    
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+console.log(personas);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Inicio></Inicio>
+          <Inicio personas={personas}></Inicio>
         </Route>
         <div>
           <Navegacion></Navegacion>
-        <Route exact path="/alumno">
-          <InicioEval></InicioEval>
-        </Route>
+          <Route exact path="/alumno">
+            <InicioEval></InicioEval>
+          </Route>
+          <Route exact path="/administrador">
+            <Admin></Admin>
+          </Route>
         </div>
       </Switch>
 
--- src/components/common/Navegacion.js
+++ src/components/common/Navegacion.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import { Image, Container, Form, Button, Col, Row } from "react-bootstrap";
+import { Navbar } from 'react-bootstrap';
+
+import {  Button, Col, Row } from "react-bootstrap";
 import isologotipo_unsta from "F:/facultad/proyecto4/front/proyecto4/src/img/isologotipo_unsta.png"
 const Navegacion = () => {
     return (
 
         <Navbar bg="secondary" className="justify-content-between align-items-center" >
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="/">
                 <img
                     src={isologotipo_unsta}
                     width="60"
                     height="60"
-                    className="d-inline-block "
+                    className="d-inline-block mr-3 "
                     alt="React Bootstrap logo"
-                />inicio
+                />INICIO
             </Navbar.Brand>
 
             <Row>
-                <Col className="align-self-center"><p className="text-center">Nombre de usuario</p></Col>
+                <Col className="align-self-center"><p className="text-center ">Nombre de usuario</p></Col>
 
-                <div className="align-self-center"><Button className="mx-2 ">Cerrar sesion</Button>
+                <div className="align-self-center"><Button href="/" className="mx-2 ">Cerrar sesion</Button>
                 </div>
             </Row>
 
